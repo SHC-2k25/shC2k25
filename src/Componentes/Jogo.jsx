1721bbb3faaa3147b838d84e7b69c813eea0ab34
SHC-2k25 <shcommunicator123@gmail.com>
2025-09-08 22:52:06 -0300

    Aa
    Aa

--- src/Componentes/Jogo.jsx
+++ src/Componentes/Jogo.jsx
@@ -3,13 +3,12 @@ import { Botao } from "./Botao.jsx"
 export function Jogo() {
   return (
     <section className="jogo">
-      <h1 data-aos="flip-left">Entre na diversão com ...</h1>
+      <h1 data-aos="flip-left">Entre na diversão com <span className="span">Whisper Uplift</span></h1>
       <div className="jogo2">
 
         <div className="jogo3" data-aos="fade-up">
           <div className="jogo4">
-            <p>NÃO ESQUECER OS "SPANS". Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
-            <p>Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. </p>
+            <p><span className="span">Peguino e Mr.Man</span> acabaram de criar a invenção mais legal de todos os tempos: o <span className="span">SH Communicator</span>, um dispositivo de comunicação assistiva para ajudar seu melhor amigo, <span className="span">Steve Howard</span>, que é portador de ELA. Mas uma organização misteriosa de vilões rouba o projeto para seus próprios planos. Agora, a dupla improvável parte numa <span className="span">aventura cheia de ação</span>, determinada a recuperar a invenção e devolver a voz de Steve!</p>
           </div>
           <Botao texto={"Jogue agora"} />
         </div>
@@ -18,7 +17,7 @@ export function Jogo() {
           width="560"
           data-aos="fade-up"
           height="315"
-          src="https://www.youtube.com/embed/x3t8NFxDgTM"
+          src="https://youtu.be/hTdoHkCxPNA?feature=shared"
           title="YouTube video player"
           frameborder="0"
           allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
