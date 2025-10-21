eea4c85882d1f0e337a7e05856d970d8aafb0cab
SHC-2k25 <shcommunicator123@gmail.com>
2025-10-20 21:46:02 -0300

    Deus é bom
    toma

--- src/Componentes/Jogo.jsx
+++ src/Componentes/Jogo.jsx
@@ -1,6 +1,11 @@
 import { Botao } from "./Botao.jsx"
 
 export function Jogo() {
+
+  const abrirJogo = () => {
+    window.open("https://whispers-uplift.vercel.app/", "_blank");
+  };
+
   return (
     <section className="jogo">
       <h1 data-aos="flip-left">Entre na diversão com <span className="span">Whisper Uplift</span></h1>
@@ -10,7 +15,7 @@ export function Jogo() {
           <div className="jogo4">
             <p><span className="span">Peguino e Mr.Man</span> acabaram de criar a invenção mais legal de todos os tempos: o <span className="span">SH Communicator</span>, um dispositivo de comunicação assistiva para ajudar seu melhor amigo, <span className="span">Steve Howard</span>, que é portador de ELA. Mas uma organização misteriosa de vilões rouba o projeto para seus próprios planos. Agora, a dupla improvável parte numa <span className="span">aventura cheia de ação</span>, determinada a recuperar a invenção e devolver a voz de Steve!</p>
           </div>
-          <Botao texto={"Jogue agora"} />
+          <Botao texto={"Jogue agora"} onClick={abrirJogo} />
         </div>
 
         <iframe
